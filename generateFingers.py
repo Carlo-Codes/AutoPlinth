18fa27a4cef0ed8d1e7b2d9f7ffc743dcbd174ee
Carlo <carlo.scrocca@googlemail.com>
2023-05-17 09:25:25 +0100

    chnaging interction tekkers

--- generateFingers.py
+++ generateFingers.py
@@ -16,24 +16,43 @@ class Generatefingers:
         rs.DeleteObjects(intersection)
 
     def makeFingerCrossSection(self, intersection):
+        surfaces = rs.ExplodePolysurfaces(intersection)
+        surfaceArea = {}
+        for surface in surfaces:
+            surfaceArea[surface] = rs.Area(surface)
+        
+        sortedSurfaceArea = sorted(surfaceArea.items(), key=operator.itemgetter(1))
+        crossSecton = sortedSurfaceArea[-1][0]
+
         intersectionEdges =  rs.DuplicateEdgeCurves(intersection)
-        if len(intersectionEdges) > 12:
-            rs.MessageBox("One of the intersections isnt a cuboid", buttons=0, title="Error")
-            pass
         edgesLength = {}
         for curve in intersectionEdges:
             edgesLength[curve] = rs.CurveLength(curve)
+
         sortedEdgeLength = sorted(edgesLength.items(), key=operator.itemgetter(1))
-        print(sortedEdgeLength)
-        point1 = rs.CurveMidPoint(sortedEdgeLength[-4][0])
-        point2 = rs.CurveMidPoint(sortedEdgeLength[-3][0])
-        point3 = rs.CurveMidPoint(sortedEdgeLength[-2][0])
-        point4 = rs.CurveMidPoint(sortedEdgeLength[-1][0])
+        edge1 = sortedEdgeLength[-1][0]
+        point1 = rs.CurveMidPoint(sortedEdgeLength[-1][0])
+
+
+
+        #intersectionEdges =  rs.DuplicateEdgeCurves(intersection)
+        #if len(intersectionEdges) > 12:
+            #rs.MessageBox("One of the intersections isnt a cuboid", buttons=0, title="Error")
+            #pass
+        #edgesLength = {}
+        #for curve in intersectionEdges:
+        #    edgesLength[curve] = rs.CurveLength(curve)
+        #sortedEdgeLength = sorted(edgesLength.items(), key=operator.itemgetter(1))
+        
+       #point1 = rs.CurveMidPoint(sortedEdgeLength[-4][0])
+        #point2 = rs.CurveMidPoint(sortedEdgeLength[-3][0])
+        #point3 = rs.CurveMidPoint(sortedEdgeLength[-2][0])
+        #point4 = rs.CurveMidPoint(sortedEdgeLength[-1][0])
     
-        crossSectionpoints = [point1,point2,point3,point4]
-        print(crossSectionpoints)
-        crossSectionCurve = rs.AddPolyline(crossSectionpoints+[crossSectionpoints[0]])
-        rs.DeleteObjects([intersectionEdges])
+        #crossSectionpoints = [point1,point2,point3,point4]
+        
+        #crossSectionsrf= rs.AddSrfPtGrid([2,2],crossSectionpoints)
+        #rs.DeleteObjects(intersectionEdges)
 
                   
 
