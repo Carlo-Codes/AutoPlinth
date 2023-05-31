da57e4b4940207da47d9a7634663ef8a1328b7ab
Carlo <carlo.scrocca@googlemail.com>
2023-05-31 17:16:18 +0100

    debugging the algorythm

--- SortPointsAroundSquare.py
+++ SortPointsAroundSquare.py
@@ -7,11 +7,25 @@ def sortPointsAroundSquare(point1, points, sortedPoints = None):
     
     measurementDict = {}
 
+    if point1 in points:
+        points.remove(point1)
+
     for point in points:
-        measurementDict[points[1]] = rs.Distance(point1, point)
+        measurementDict[point] = rs.Distance(point1, point)
+    
+    print (measurementDict)
 
     sortedMeasurmentDict = sorted(measurementDict.items(), key=operator.itemgetter(1))
-    sortedPoints.append(sortedMeasurmentDict[0])
-    points.remove(sortedMeasurmentDict[0])
+    closestPoint = sortedMeasurmentDict[0][0]
+    sortedPoints.append(closestPoint)
+    points.remove(closestPoint)
     
-    sortPointsAroundSquare(sortedMeasurmentDict[0], points, sortedPoints)
+    sortPointsAroundSquare(closestPoint, points, sortedPoints)
+    return sortedPoints
+
+points = rs.GetObjects("sel Points", 1)
+
+sortedPoints = sortPointsAroundSquare(points[0], points)
+
+rs.AddPolyline(sortedPoints)
+
