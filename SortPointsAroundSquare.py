d915e94a83a6fdacb4eb8da476088bef54768715
Carlo <carlo.scrocca@googlemail.com>
2023-06-01 09:45:12 +0100

    algoworking, just realised its a shortest distance

--- SortPointsAroundSquare.py
+++ SortPointsAroundSquare.py
@@ -4,16 +4,23 @@ import operator
 def sortPointsAroundSquare(point1, points, sortedPoints = None):
     if sortedPoints == None:
         sortedPoints = []
+        sortedPoints.append(points[0])
+    
+    if len(points) == 1:
+        sortedPoints.append(points[0])
+        points.remove(points[0])
+        return sortedPoints
+    
     
     measurementDict = {}
 
     if point1 in points:
         points.remove(point1)
+        
 
     for point in points:
         measurementDict[point] = rs.Distance(point1, point)
     
-    print (measurementDict)
 
     sortedMeasurmentDict = sorted(measurementDict.items(), key=operator.itemgetter(1))
     closestPoint = sortedMeasurmentDict[0][0]
@@ -26,6 +33,7 @@ def sortPointsAroundSquare(point1, points, sortedPoints = None):
 points = rs.GetObjects("sel Points", 1)
 
 sortedPoints = sortPointsAroundSquare(points[0], points)
+print(len(sortedPoints))
 
 rs.AddPolyline(sortedPoints)
 
